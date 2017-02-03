bcf4a7b3d14f2d3a8ca9f581f0db51ad29f561a3
Kerry Scharfglass <iborgel@gmail.com>
2017-02-02 21:18:23 -0800

    Move SPI1 SCK to PB3

--- Firmware/DiscoBoard-Test/Sympetrum-DiscoBoardTest/Src/main.c
+++ Firmware/DiscoBoard-Test/Sympetrum-DiscoBoardTest/Src/main.c
@@ -184,6 +184,7 @@ static void MX_GPIO_Init(void)
 
    /* GPIO Ports Clock Enable */
    __HAL_RCC_GPIOA_CLK_ENABLE();
+   __HAL_RCC_GPIOB_CLK_ENABLE();
    __HAL_RCC_GPIOC_CLK_ENABLE();
 
    /*Configure GPIO pin Output Level */
--- Firmware/DiscoBoard-Test/Sympetrum-DiscoBoardTest/Src/stm32f0xx_hal_msp.c
+++ Firmware/DiscoBoard-Test/Sympetrum-DiscoBoardTest/Src/stm32f0xx_hal_msp.c
@@ -76,14 +76,21 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* hspi)
   
     /**SPI1 GPIO Configuration    
     PA5     ------> SPI1_SCK
-    PA7     ------> SPI1_MOSI 
+    PB3     ------> SPI1_MOSI 
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_7;
+    GPIO_InitStruct.Pin = GPIO_PIN_5;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF0_SPI1;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_3;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF0_SPI1;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /* USER CODE BEGIN SPI1_MspInit 1 */
 
@@ -107,7 +114,8 @@ void HAL_SPI_MspDeInit(SPI_HandleTypeDef* hspi)
     PA5     ------> SPI1_SCK
     PA7     ------> SPI1_MOSI 
     */
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_5|GPIO_PIN_7);
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_5);
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_3);
 
   }
   /* USER CODE BEGIN SPI1_MspDeInit 1 */
