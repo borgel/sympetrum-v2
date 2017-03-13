e8a80e70d20f01df141cc00919e21755890caf08
Kerry Scharfglass <iborgel@gmail.com>
2017-03-12 23:57:07 -0700

    Add most basic LED control infrastructure

---
+++ Firmware/Inc/led.h
@@ -0,0 +1,21 @@
+#ifndef LED_H__
+#define LED_H__
+
+/*
+ * A thin driver for the APA102's used together in a group.
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "color.h"
+#include "stm32f0xx_hal.h"
+
+#define MAX_LED_CHAIN_LENGTH       10
+
+bool led_Init(SPI_HandleTypeDef spiBus);
+bool led_SetChannel(uint8_t ch, struct color_ColorRGB color);
+bool led_UpdateChannels(void);
+
+#endif//LED_H__
+
