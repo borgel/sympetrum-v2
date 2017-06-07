86b9f8bd46dc27994a67f4136595f56d0535878a
Kerry Scharfglass <iborgel@gmail.com>
2017-06-06 22:31:03 -0700

    IR Decode cleanup

--- Firmware/Src/rc5_decode.c
+++ Firmware/Src/rc5_decode.c
@@ -1,20 +1,5 @@
 /**
   TODO update this block
- ******************************************************************************
- * Notes
- * -------------------------
- *      - TIM2 Capture Compare and Update interrupts are used to decode the RC5
- *        frame, if a frame is received correctly a global variable RC5FrameReceived 
- *        will be set to inform the application.
- *
- *      - The application should call the function RC5_Decode() to retrieve 
- *        the received RC5 frame.
- *
- *      - TIM2_IRQHandler  ISR is used by this driver.
- *        If you are using one or both Interrupts in your application you have 
- *        to proceed carefully:
- *           - either add your application code in these ISRs
- *           - or copy the contents of these ISRs in you application code
  */
 
 #include "rc5_decode.h"
@@ -26,175 +11,6 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-
-//TODO rm these tables?
-/* RC5 address table */
-static char* const rc5_devices[32] = {
-   "       TV1          ",                  /*  0 */
-   "       TV2          ",                  /*  1 */
-   "    Video Text      ",                  /*  2 */
-   "   Extension TV     ",                  /*  3 */
-   "  LaserVideoPlayer  ",                  /*  4 */
-   "       VCR1         ",                  /*  5 */
-   "       VCR2         ",                  /*  6 */
-   "      Reserved      ",                  /*  7 */
-   "       Sat1         ",                  /*  8 */
-   "   Extension VCR    ",                  /*  9 */
-   "       Sat2         ",                  /* 10 */
-   "     Reserved       ",                  /* 11 */
-   "     CD Video       ",                  /* 12 */
-   "      Reserved      ",                  /* 13 */
-   "     CD Photo       ",                  /* 14 */
-   "      Reserved      ",                  /* 15 */
-   " Preampli Audio 1   ",                  /* 16 */
-   "      Tuner         ",                  /* 17 */
-   "  Analog Magneto    ",                  /* 18 */
-   " Preampli Audio 2   ",                  /* 19 */
-   "       CD           ",                  /* 20 */
-   "    Rack Audio      ",                  /* 21 */
-   " Audio Sat Receiver ",                  /* 22 */
-   "   DDC Magneto      ",                  /* 23 */
-   "     Reserved       ",                  /* 24 */
-   "     Reserved       ",                  /* 25 */
-   "      CDRW          ",                  /* 26 */
-   "     Reserved       ",                  /* 27 */
-   "     Reserved       ",                  /* 28 */
-   "     Reserved       ",                  /* 29 */
-   "     Reserved       ",                  /* 30 */
-   "     Reserved       "                   /* 31 */
-};
-
-/* RC5 commands table*/
-static char* const rc5_Commands[128] = {
-   "       Num0         ",                                       /* 0 */
-   "       Num1         ",                                       /* 1 */
-   "       Num2         ",                                       /* 2 */
-   "       Num3         ",                                       /* 3 */
-   "       Num4         ",                                       /* 4 */
-   "       Num5         ",                                       /* 5 */
-   "       Num6         ",                                       /* 6 */
-   "       Num7         ",                                       /* 7 */
-   "       Num8         ",                                       /* 8 */
-   "       Num9         ",                                       /* 9 */
-   "     TV Digits      ",                                       /* 10 */
-   "      TV Freq       ",                                       /* 11 */
-   "     TV StandBy     ",                                       /* 12 */
-   "  TV Mute On-Off    ",                                       /* 13 */
-   "   TV Preference    ",                                       /* 14 */
-   "    TV Display      ",                                       /* 15 */
-   "    Volume Up       ",                                       /* 16 */
-   "    Volume Down     ",                                       /* 17 */
-   "    Brightness Up   ",                                       /* 18 */
-   "   Brightness Down  ",                                       /* 19 */
-   " Color Saturation Up",                                       /* 20 */
-   "ColorSaturation Down",                                       /* 21 */
-   "      Bass Up       ",                                       /* 22 */
-   "      Bass Down     ",                                       /* 23 */
-   "      Treble Up     ",                                       /* 24 */
-   "     Treble Down    ",                                       /* 25 */
-   "    Balance Right   ",                                       /* 26 */
-   "    BalanceLeft     ",                                       /* 27 */
-   "   TV Contrast Up   ",                                       /* 28 */
-   "  TV Contrast Down  ",                                       /* 29 */
-   "   TV Search Up     ",                                       /* 30 */
-   "  TV tint-hue Down  ",                                       /* 31 */
-   "   TV CH Prog Up    ",                                       /* 32 */
-   "   TV CH ProgDown   ",                                       /* 33 */
-   "TV Last prog-channel",                                       /* 34 */
-   " TV Select language ",                                       /* 35 */
-   " TV Spacial Stereo  ",                                       /* 36 */
-   "  TV Stereo Mono    ",                                       /* 37 */
-   "  TV Sleep Timer    ",                                       /* 38 */
-   "   TV tint-hue Up   ",                                       /* 39 */
-   "   TV RF Switch     ",                                       /* 40 */
-   "   TV Store-VOTE    ",                                       /* 41 */
-   "      TV Time       ",                                       /* 42 */
-   "   TV Scan Fwd Inc  ",                                       /* 43 */
-   "    TV Decrement    ",                                       /* 44 */
-   "      Reserved      ",                                       /* 45 */
-   "   TV control-menu  ",                                       /* 46 */
-   "    TV Show Clock   ",                                       /* 47 */
-   "      TV Pause      ",                                       /* 48 */
-   "   TV Erase Entry   ",                                       /* 49 */
-   "     TV Rewind      ",                                       /* 50 */
-   "     TV Goto        ",                                       /* 51 */
-   "     TV Wind        ",                                       /* 52 */
-   "     TV Play        ",                                       /* 53 */
-   "     TV Stop        ",                                       /* 54 */
-   "     TV Record      ",                                       /* 55 */
-   "    TV External 1   ",                                       /* 56 */
-   "    TV External 2   ",                                       /* 57 */
-   "     Reserved       ",                                       /* 58 */
-   "     TV Advance     ",                                       /* 59 */
-   "   TV TXT-TV toggle ",                                       /* 60 */
-   "  TV System StandBy ",                                       /* 61 */
-   "TV Picture Crispener",                                       /* 62 */
-   "    System Select   ",                                       /* 63 */
-   "     Reserved       ",                                       /* 64 */
-   "     Reserved       ",                                       /* 65 */
-   "     Reserved       ",                                       /* 66 */
-   "     Reserved       ",                                       /* 67 */
-   "     Reserved       ",                                       /* 68 */
-   "     Reserved       ",                                       /* 69 */
-   "  TV Speech Music   ",                                       /* 70 */
-   "  DIM Local Display ",                                       /* 71 */
-   "     Reserved       ",                                       /* 72 */
-   "     Reserved       ",                                       /* 73 */
-   "     Reserved       ",                                       /* 74 */
-   "     Reserved       ",                                       /* 75 */
-   "     Reserved       ",                                       /* 76 */
-   "Inc Control Setting ",                                       /* 77 */
-   "Dec Control Setting ",                                       /* 78 */
-   "   TV Sound Scroll  ",                                       /* 79 */
-   "      Step Up       ",                                       /* 80 */
-   "     Step Down      ",                                       /* 81 */
-   "      Menu On       ",                                       /* 82 */
-   "      Menu Off      ",                                       /* 83 */
-   "     AV Status      ",                                       /* 84 */
-   "      Step Left     ",                                       /* 85 */
-   "      Step Right    ",                                       /* 86 */
-   "     Acknowledge    ",                                       /* 87 */
-   "      PIP On Off    ",                                       /* 88 */
-   "      PIP Shift     ",                                       /* 89 */
-   "    PIP Main Swap   ",                                       /* 90 */
-   "    Strobe On Off   ",                                       /* 91 */
-   "     Multi Strobe   ",                                       /* 92 */
-   "      Main Frozen   ",                                       /* 93 */
-   "  3div9 Multi scan  ",                                       /* 94 */
-   "      PIPSelect     ",                                       /* 95 */
-   "      MultiPIP      ",                                       /* 96 */
-   "     Picture DNR    ",                                       /* 97 */
-   "     Main Stored    ",                                       /* 98 */
-   "     PIP Strobe     ",                                       /* 99 */
-   "    Stored Picture  ",                                       /* 100 */
-   "      PIP Freeze    ",                                       /* 101 */
-   "      PIP Step Up   ",                                       /* 102 */
-   "    PIP Step Down   ",                                       /* 103 */
-   "    TV PIP Size     ",                                       /* 104 */
-   "  TV Picture Scroll ",                                       /* 105 */
-   " TV Actuate Colored ",                                       /* 106 */
-   "       TV Red       ",                                       /* 107 */
-   "       TV Green     ",                                       /* 108 */
-   "      TV Yellow     ",                                       /* 109 */
-   "      TV Cyan       ",                                       /* 110 */
-   "    TV Index White  ",                                       /* 111 */
-   "      TV Next       ",                                       /* 112 */
-   "     TV Previous    ",                                       /* 113 */
-   "      Reserved      ",                                       /* 114 */
-   "      Reserved      ",                                       /* 115 */
-   "      Reserved      ",                                       /* 116 */
-   "      Reserved      ",                                       /* 117 */
-   "      Sub Mode      ",                                       /* 118 */
-   "   Option Sub Mode  ",                                       /* 119 */
-   "      Reserved      ",                                       /* 120 */
-   "      Reserved      ",                                       /* 121 */
-   "TV Store Open Close ",                                       /* 122 */
-   "      Connect       ",                                       /* 123 */
-   "     Disconnect     ",                                       /* 124 */
-   "      Reserved      ",                                       /* 125 */
-   "  TV Movie Expand   ",                                       /* 126 */
-   "  TV Parental Access"                                        /* 127 */
-};
 
 #define RC5_1T_TIME                          0x00
 #define RC5_2T_TIME                          0x01
@@ -209,7 +25,7 @@ static char* const rc5_Commands[128] = {
 /* Packet struct for reception*/
 #define RC5_PACKET_STATUS_EMPTY              1<<0
 
-#define TIM_PRESCALER          47                       /* !< TIM prescaler */
+#define TIM_PRESCALER                        47                       /* !< TIM prescaler */
 
 typedef struct
 {
@@ -382,8 +198,7 @@ bool RC5_Decode(RC5_Frame_TypeDef *rc5_frame)
    /* If frame received */
    if(RC5FrameReceived)
    {
-
-      RC5_Data = RC5TmpPacket.data ;
+      RC5_Data = RC5TmpPacket.data;
 
       /* RC5 frame field decoding */
       rc5_frame->Address = (RC5TmpPacket.data >> 6) & 0x1F;
@@ -391,15 +206,13 @@ bool RC5_Decode(RC5_Frame_TypeDef *rc5_frame)
       rc5_frame->FieldBit = (RC5TmpPacket.data >> 12) & 0x1;
       rc5_frame->ToggleBit = (RC5TmpPacket.data >> 11) & 0x1;
 
+      iprintf("Raw = 0x%x\r\n", RC5TmpPacket.data);
+
       /* Check if command ranges between 64 to 127:Upper Field */
       if (rc5_frame->FieldBit == 0x00)
       {
          rc5_frame->Command =  (1<<6)| rc5_frame->Command; 
       }
-
-      /* Display RC5 message */
-      iprintf("Command:%s\r\n", rc5_Commands[rc5_frame->Command]);
-      iprintf("Device:%s\r\n", rc5_devices[rc5_frame->Address]);
 
       /* Default state */
       RC5FrameReceived = false;
