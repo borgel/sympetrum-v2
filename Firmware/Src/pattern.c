7a3580310b504208c305dd733242b6a15e4efee8
Kerry Scharfglass <iborgel@gmail.com>
2017-06-26 22:36:56 -0700

    Rewrite with double clock system, might work?

--- Firmware/Src/pattern.c
+++ Firmware/Src/pattern.c
@@ -18,29 +18,39 @@ The way real fireflies do it: ncase.me/fireflies/
 #include "iprintf.h"
 #include <stdint.h>
 
-static const uint16_t ClockIntervalRampMS[] = {5000, 2500, 1250, 625};
+// Get the period of the hue clock for a given beacon interval
+#define HUE_PERIOD_MS_FOR_BEACON(x)       ((x) / 255)
 
-// amount to bump the clock forward when a beacon is seen
-static const uint8_t ClockBump = 255/10;
+// Parallel arrays used to set clock intervals
+static const uint16_t BeaconIntervalRampMS[] = {5000, 2500, 1250, 625};
 
 // STATE STUFF
-// Fast hue clock
+// Fast hue clock. The period is = the time between ticks of the Beacon Clock.
+// That means it needs to tick 255 times during the Beacon interval
 static uint8_t HueClock;
+static uint16_t HueClockPeriod;
+static uint32_t LastHueClockTime;
+
 // Slow beacon clock
 static uint16_t BeaconClock;
-
-static uint16_t ClockIntervalMS;
-static uint32_t LastClockTimeMS;
+static uint16_t BeaconClockInterval;
+static uint16_t BeaconClockRampPosition;
+static uint32_t LastBeaconClockTime;
 
 static void pattern_Permute(void);
 
 void pattern_Init(void) {
+   BeaconClockRampPosition = 0;
    BeaconClock = 0;
-   HueClock = 0;
-   ClockIntervalMS = ClockIntervalRampMS[0] / 100;
-   LastClockTimeMS = 0;
+   BeaconClockInterval = BeaconIntervalRampMS[BeaconClockRampPosition];
+   // Start one tick in to allow for time manipulation
+   LastBeaconClockTime = BeaconClockInterval;
 
-   //TODO pass in a CB for each RX'd beacon
+   HueClock = 0;
+   HueClockPeriod = HUE_PERIOD_MS_FOR_BEACON(BeaconClockInterval);
+   LastHueClockTime = HueClockPeriod;
+
+   //TODO pass in a CB for each RX'd beacon?
    beacon_Init();
 
    //FIXME en
@@ -58,36 +68,54 @@ void pattern_GiveTime(uint32_t const systimeMS) {
    // Adjust the pattern based on any beacons, etc
    pattern_Permute();
 
-   // On Beacon
-   if(systimeMS - LastClockTimeMS > ClockIntervalMS) {
-      // Max S and Max V are brightest colors
-      struct color_ColorHSV c = {.h = HueClock, .s = 255, .v = 255};
+   // XXX On Beacon tick (infrequent)
+   if(systimeMS - LastBeaconClockTime > BeaconClockInterval) {
+      LastBeaconClockTime = systimeMS;
 
-      LastClockTimeMS = systimeMS;
-
+      //FIXME don't send hue, that doesn't matter
       iprintf("Beaconing (Hue %d)\n", HueClock);
 
       beacon_Send(HueClock);
 
-      // now set LEDs to that color
-      //TODO modify animation bias instead of raw hue set
-      for(int i = 0; i < 10; i++) {
-         led_SetChannel(i, c);
+      // Reset Hue clock too
+      HueClock = 0;
+      LastHueClockTime = systimeMS;
+
+      // TODO regress beacon interval ramp to slow it down
+   }
+
+   //XXX On Hue tick (frequent)
+   if(systimeMS - LastHueClockTime > HueClockPeriod) {
+      LastHueClockTime = systimeMS;
+
+      iprintf("H", HueClock);
+
+      //TODO set animation bias to HueClock instead of raw hue set
+
+      {
+         // Max S and Max V are brightest colors
+         struct color_ColorHSV c = {.h = HueClock, .s = 255, .v = 255};
+
+         // now set LEDs to that color
+         for(int i = 0; i < 10; i++) {
+            led_SetChannel(i, c);
+         }
       }
 
-      // uhh, how do we do this?
+      // this is the 'actual' tick
       HueClock++;
    }
 }
 
 static void pattern_Permute(void) {
    //TODO
-   //uhh, is there actually anything to do here?
 }
 
 void pattern_SawBeacon(uint16_t rawBeacon) {
-   // Dumbest choice - bump the clock
-   HueClock += ClockBump;
-
    //TODO react to incoming beacon in complex way
+   // FIXME how much should we jump the clock forward? X% of interval?
+   // Moving last time back is == moving next time sooner
+   LastHueClockTime -= 50;
+
+   //TODO advance beacon interval ramp to speed it up?
 }
